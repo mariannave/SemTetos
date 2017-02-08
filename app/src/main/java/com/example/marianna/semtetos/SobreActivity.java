a15ec2e46de21dfe601eb9dd6b1a57bb99f7e312
Marianna Veríssimo <marianna1204@gmail.com>
2017-02-08 12:07:42 -0300

    Add classe da página Sobre

---
+++ app/src/main/java/com/example/marianna/semtetos/SobreActivity.java
@@ -0,0 +1,16 @@
+package com.example.marianna.semtetos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marianna on 08/02/2017.
+ */
+
+public class SobreActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
