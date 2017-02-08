0839b6a8b20689c4af711e8f03ec8c6a4adb3b20
Marianna Veríssimo <marianna1204@gmail.com>
2017-02-08 12:11:02 -0300

    Add model Anuncio

---
+++ app/src/main/java/com/example/marianna/semtetos/model/Anuncio.java
@@ -0,0 +1,85 @@
+package com.example.marianna.semtetos.model;
+
+/**
+ * Created by Marianna on 08/02/2017.
+ */
+
+public class Anuncio {
+
+    private int id;
+    private String tipoDoAnuncio;
+    private String titulo;
+    private String descricao;
+    private String anunciante;
+    private String contato;
+    private String imagemURL;
+
+
+    public Anuncio(String tipoDoAnuncio, String titulo, String descricao, String anunciante, String contato, String imagemURL) {
+        this.tipoDoAnuncio = tipoDoAnuncio;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.anunciante = anunciante;
+        this.contato = contato;
+        this.imagemURL = imagemURL;
+    }
+
+    public Anuncio(int id, String tipoDoAnuncio, String titulo, String descricao, String anunciante, String contato, String imagemURL) {
+        this.id = id;
+        this.tipoDoAnuncio = tipoDoAnuncio;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.anunciante = anunciante;
+        this.contato = contato;
+        this.imagemURL = imagemURL;
+    }
+
+    public String getTipoDoAnuncio() {
+        return tipoDoAnuncio;
+    }
+
+    public void setTipoDoAnuncio(String tipoDoAnuncio) {
+        this.tipoDoAnuncio = tipoDoAnuncio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getAnunciante() {
+        return anunciante;
+    }
+
+    public void setAnunciante(String anunciante) {
+        this.anunciante = anunciante;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getImagemURL() {
+        return imagemURL;
+    }
+
+    public void setImagemURL(String imagemURL) {
+        this.imagemURL = imagemURL;
+    }
+
+}
