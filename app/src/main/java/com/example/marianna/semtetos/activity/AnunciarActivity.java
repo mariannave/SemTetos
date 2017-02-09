3fa09f6e5a8b5ac2e99a968f570686d6dc637bb6
Marianna Veríssimo <marianna1204@gmail.com>
2017-02-09 14:10:53 -0300

    Criado package dos activity

--- app/src/main/java/com/example/marianna/semtetos/activity/AnunciarActivity.java
+++ app/src/main/java/com/example/marianna/semtetos/activity/AnunciarActivity.java
@@ -1,12 +1,14 @@
-package com.example.marianna.semtetos;
+package com.example.marianna.semtetos.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SobreActivity extends AppCompatActivity{
+import com.example.marianna.semtetos.R;
+
+public class AnunciarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sobre);
+        setContentView(R.layout.activity_anunciar);
     }
 }
